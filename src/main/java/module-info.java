d579a77bf59916571343fd2b4497eaaf9371aaa5
Nick <houdenicholas00@gmail.com>
2021-08-12 13:49:19 -0600

    ehh

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.iswproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.iswproject to javafx.fxml;
-    exports com.example.iswproject;
-}
